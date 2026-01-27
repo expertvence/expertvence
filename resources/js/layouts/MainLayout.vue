dccff50389313895bde5572b8790e150e8e6e9a8
mofazzal-phy <mofazzal.phy@gmail.com>
2026-01-27 22:21:34 +0600

    update favicon

--- resources/js/layouts/MainLayout.vue
+++ resources/js/layouts/MainLayout.vue
@@ -2,7 +2,7 @@
   <TopBar />
   <Navbar />
 
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!--   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <link
   rel="stylesheet"
@@ -12,7 +12,7 @@
 <link
   rel="stylesheet"
   href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-/>
+/> -->
 
 
   <main>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -8,4 +8,31 @@
 <body>
     <div id="app"></div>
 </body>
+</html> --}}
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <title>ExpertVence</title>
+
+    <!-- ✅ FAVICON -->
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
+
+    <!-- ✅ Viewport -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- ✅ Font Awesome -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+    >
+
+    @vite(['resources/js/app.js'])
+</head>
+<body>
+    <div id="app"></div>
+</body>
 </html>
+
