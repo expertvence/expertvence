cc4b71e7d1f509a6a11fd6a002ca2387572aed1a
mofazzal-phy <mofazzal.phy@gmail.com>
2026-01-31 13:59:07 +0600

    satarday 2nd update

--- resources/js/components/TopBar.vue
+++ resources/js/components/TopBar.vue
@@ -2,15 +2,16 @@
   <div class="topbar" :class="{ 'topbar-hide': hideTopbar }">
     <!-- LEFT INFO -->
     <div class="topbar-left">
-      <span class="info-item">
-        🇧🇩 <strong>+880 1813590326</strong>
-      </span>
-
-
-      <span class="dot"></span>
+      <span class="flag bd"></span> +880 1813590326
 
       <span class="info-item email">
-        ✉ contact@expertvence.com
+        <!-- Titan Email Icon (simplified SVG) -->
+        <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect x="1" y="4" width="22" height="16" rx="2" fill="#4368F0" />
+          <path d="M1 4L12 13L23 4" stroke="#fff" stroke-width="2" />
+          <path d="M1 20V8L12 17L23 8V20H1Z" fill="#fff" />
+        </svg>
+        info@expertvence.com
       </span>
     </div>
 
--- resources/js/layouts/MainLayout.vue
+++ resources/js/layouts/MainLayout.vue
@@ -1,21 +1,7 @@
 <template>
   <TopBar />
   <Navbar />
-
-<!--   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-  <link
-  rel="stylesheet"
-  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-/>
-
-<link
-  rel="stylesheet"
-  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-/> -->
-
-
-  <main>
+  <main class="page-content">
     <router-view />
   </main>
   <Footer />
